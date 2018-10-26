e2aa2edfce4ac912c404bc311482b924cb3cf4ed
tiffanyrudd <you@example.com>
2018-10-26 01:36:03 -0700

    updated the read me file to desktop

---
+++ javascript.js
@@ -0,0 +1,3 @@
+additional java script info to add… https://blog.bitsrc.io/11-javascript-animation-libraries-for-2018-9d7ac93a2c59
+
+picture: wheat field at sunset: https://www.google.com/search?q=wheat+field+sunset&rlz=1C1CHBF_enUS815US816&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiHvfeby57eAhUEHTQIHbzWD6kQ_AUIDigB&biw=1466&bih=654#imgrc=TqXgqOjAgB_H0M:
